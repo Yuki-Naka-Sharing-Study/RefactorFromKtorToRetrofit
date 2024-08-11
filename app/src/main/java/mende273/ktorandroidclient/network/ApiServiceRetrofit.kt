d47b3a2c0de6ab83eac9212a7034bc6737e1a71c
Yuki Naka <yukinaka2168@au.com>
2024-08-11 14:43:17 +0900

    ApiServiceRetrofit.ktの「BASE_URL」を「"https://www.thecocktaildb.com/"」に変更。 「getDrinks」の「@GET」以降を「"api/json/v1/1/filter.php?c=Ordinary_Drink"」に変更。

--- app/src/main/java/mende273/ktorandroidclient/network/ApiServiceRetrofit.kt
+++ app/src/main/java/mende273/ktorandroidclient/network/ApiServiceRetrofit.kt
@@ -5,8 +5,8 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiServiceRetrofit {
-    @GET("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink")
+    @GET("api/json/v1/1/filter.php?c=Ordinary_Drink")
     fun getDrinks(): Call<List<DrinksResponse>>
 }
 
-const val BASE_URL = "base_url"
+const val BASE_URL = "https://www.thecocktaildb.com/"
