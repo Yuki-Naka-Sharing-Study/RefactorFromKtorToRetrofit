d50fe80fba6a930f83680db4a492ff285dee3cb8
Yuki Naka <yukinaka2168@au.com>
2024-08-12 12:37:11 +0900

    「ApiService.kt」を修正。

--- app/src/main/java/mende273/ktorandroidclient/network/ApiService.kt
+++ app/src/main/java/mende273/ktorandroidclient/network/ApiService.kt
@@ -1,14 +1,9 @@
-package mende273.ktorandroidclient.network
+package mende273.retrofitandroidclient.network
 
-import io.ktor.client.HttpClient
-import io.ktor.client.request.get
+import mende273.ktorandroidclient.data.model.DrinksResponse
+import retrofit2.http.GET
 
-class ApiService(private val client: HttpClient) {
-
-    companion object {
-        private const val END_POINT =
-            "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink"
-    }
-
-    suspend fun getDrinks() = client.get(END_POINT)
+interface ApiService {
+    @GET("api/json/v1/1/filter.php?c=Ordinary_Drink")
+    suspend fun getDrinks(): DrinksResponse
 }
