f6bcd90a4ba8ccebefc7c5ea08734cf408a06e4e
Yuki Naka <yukinaka2168@au.com>
2024-08-12 12:46:34 +0900

    「RetrofitApplication.kt」に名前を改名。

--- app/src/main/java/mende273/ktorandroidclient/RetrofitApplication.kt
+++ app/src/main/java/mende273/ktorandroidclient/RetrofitApplication.kt
@@ -1,16 +1,15 @@
-package mende273.ktorandroidclient
+package mende273.retrofitandroidclient
 
 import android.app.Application
 import mende273.ktorandroidclient.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class KtorApplication : Application() {
-
+class RetrofitApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidContext(this@KtorApplication)
+            androidContext(this@RetrofitApplication)
             modules(appModule)
         }
     }
