194338a79e613f02a0292090f48e137fb58fb3ce
Yuki Naka <yukinaka2168@au.com>
2024-08-12 12:30:48 +0900

    「moshi」をimplementation。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,8 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
