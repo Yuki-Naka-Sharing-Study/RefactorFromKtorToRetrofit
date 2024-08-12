6bbfd8ce13561369d1dd699d1774c2a463309d6f
Yuki Naka <yukinaka2168@au.com>
2024-08-12 12:51:50 +0900

    「implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")」をimplementation。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
-
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
 
     // lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
