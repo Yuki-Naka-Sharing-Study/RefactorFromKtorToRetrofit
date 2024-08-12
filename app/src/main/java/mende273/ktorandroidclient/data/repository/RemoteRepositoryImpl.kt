9c26e9631d52ffcce48120eee80f638713a160cf
Yuki Naka <yukinaka2168@au.com>
2024-08-12 12:35:59 +0900

    「RemoteRepository」を少し修正。

--- app/src/main/java/mende273/ktorandroidclient/data/repository/RemoteRepositoryImpl.kt
+++ app/src/main/java/mende273/ktorandroidclient/data/repository/RemoteRepositoryImpl.kt
@@ -1,15 +1,7 @@
-package mende273.ktorandroidclient.data.repository
+package mende273.retrofitandroidclient.data.repository
 
-import io.ktor.client.call.body
 import mende273.ktorandroidclient.data.model.Drink
-import mende273.ktorandroidclient.data.model.DrinksResponse
-import mende273.ktorandroidclient.network.ApiService
 
-class RemoteRepositoryImpl(
-    private val apiService: ApiService
-) : RemoteRepository {
-    override suspend fun getDrinks(): Result<List<Drink>> =
-        runCatching {
-            apiService.getDrinks().body<DrinksResponse>().drinks
-        }
+interface RemoteRepository {
+    suspend fun getDrinks(): Result<List<Drink>>
 }
